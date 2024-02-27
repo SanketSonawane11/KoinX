a7957713c028e98c6c71d889c53575c1d5a19440
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-02-27 10:13:18 +0530

    Minor Ui tweaks

--- src/app/Components/Aboutbitcoin.js
+++ src/app/Components/Aboutbitcoin.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Horizontalaction from './Horizontalaction'
+// import Horizontalaction from './Horizontalaction'
+import { FaArrowRightLong } from "react-icons/fa6";
 
 function Aboutbitcoin() {
     return (
@@ -26,7 +27,26 @@ function Aboutbitcoin() {
                     <h1 className='font-semibold text-[24px] mb-[20px]'>
                         Already Holding Bitcoin?
                     </h1>
-                    <Horizontalaction />
+                    <div className='horizontal-action mb-[50px]'>
+                        <div className='horizontal-card one'>
+                            <img src='/one.png'></img>
+                            <div className='content'>
+                                <h2>Calculate your Profits</h2>
+                                <button className='already-button'>
+                                    Check Now <FaArrowRightLong />
+                                </button>
+                            </div>
+                        </div>
+                        <div className='horizontal-card two'>
+                            <img src='/two.png'></img>
+                            <div className='content'>
+                                <h2>Calculate your tax liability</h2>
+                                <button className='already-button'>
+                                    Check Now <FaArrowRightLong />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                     <hr />
                     <p>Fermentum hendrerit imperdiet nulla viverra faucibus. Sit aliquam massa vel convallis duis ac. Mi adipiscing semper scelerisque porttitor pulvinar nunc risus. Fermentum potenti iaculis lacinia congue ipsum fames amet dui. Purus ultrices tincidunt volutpat in eget. Ullamcorper dui
                     </p>
--- src/app/Components/Horizontalaction.js
+++ src/app/Components/Horizontalaction.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import { FaArrowRightLong } from "react-icons/fa6";
+// import React from 'react'
+// import { FaArrowRightLong } from "react-icons/fa6";
 
 
-function Horizontalaction() {
-    return (
-        <div className='horizontal-action'>
-            <div className='horizontal-card one'>
-                <img src='/one.png'></img>
-                <div className='content'>
-                    <h2>Calculate your Profits</h2>
-                    <button className='already-button'>
-                        Check Now <FaArrowRightLong />
-                    </button>
-                </div>
-            </div>
-            <div className='horizontal-card two'>
-                <img src='/two.png'></img>
-                <div className='content'>
-                    <h2>Calculate your tax liability</h2>
-                    <button className='already-button'>
-                        Check Now <FaArrowRightLong />
-                    </button>
-                </div>
-            </div>
-        </div>
-    )
-}
+// function Horizontalaction() {
+//     return (
+//         // <div className='horizontal-action'>
+//         //     <div className='horizontal-card one'>
+//         //         <img src='/one.png'></img>
+//         //         <div className='content'>
+//         //             <h2>Calculate your Profits</h2>
+//         //             <button className='already-button'>
+//         //                 Check Now <FaArrowRightLong />
+//         //             </button>
+//         //         </div>
+//         //     </div>
+//         //     <div className='horizontal-card two'>
+//         //         <img src='/two.png'></img>
+//         //         <div className='content'>
+//         //             <h2>Calculate your tax liability</h2>
+//         //             <button className='already-button'>
+//         //                 Check Now <FaArrowRightLong />
+//         //             </button>
+//         //         </div>
+//         //     </div>
+//         // </div>
+//     )
+// }
 
-export default Horizontalaction
+// export default Horizontalaction
