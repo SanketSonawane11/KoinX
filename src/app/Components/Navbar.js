2799c6f4ea8163ff2e0ff467b34c589e3c849d17
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-02-27 20:15:38 +0530

    Partially fixed hamburger menu

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -7,7 +7,6 @@ import React, { useState } from 'react'
 function Navbar() {
 
     const menuClasses = 'menu-items';
-    const innerMenuClasses = 'inner-menu';
     const getStartedClasses = 'get-started';
 
     // Function to toggle mobile menu
@@ -19,21 +18,22 @@ function Navbar() {
     return (
         <div className='nav-main'>
             <div className='logo'></div>
-            <div className={menuClasses}>
-                <ul className={innerMenuClasses}>
+            <div className='menu-items flex flex-col h-[200px] justify-between'> {/*menu-items*/}
+                <ul className='inner-menu flex flex-col h-[fit] gap-[10px] bg-slate-50'> {/*inner-menu*/}
                     <li><Link href="#">Crypto Taxes</Link></li>
                     <li><Link href="#">Free Tools</Link></li>
                     <li><Link href="#">Resource Center</Link></li>
                 </ul>
-                <div className={getStartedClasses}>
+                <div className={getStartedClasses}> {/*get-started*/}
                     <button className="w-[136px] h-[40px]">Get Started</button>
                 </div>
-                <div className="hamburger" onClick={toggleMobileMenu}>
+                
+            </div>
+            <div className="hamburger" onClick={toggleMobileMenu}>
                     <div></div>
                     <div></div>
                     <div></div>
                 </div>
-            </div>
         </div>
     )
 }
